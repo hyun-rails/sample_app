d405e1f02335efccc360e4644fab4a2758139664
hyunrails <hyun1@swarthmore.edu>
2014-01-05 08:56:25 -0500

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  #let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
@@ -31,7 +37,14 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+  end
+
 end
